adf411f154bcb4ca6fa53d7ba9c5ed5b1a338abc
Willy Huang <willyhuang18@gmail.com>
2022-09-28 12:47:32 -0700

    adjust the project card

--- will-portfolio/components/Projects.tsx
+++ will-portfolio/components/Projects.tsx
@@ -32,8 +32,8 @@ function Projects({projects}: Props) {
             transition={{duration: 1.2}}
             whileInView={{opacity:1, y:0}}
             viewport={{once: true}}
-            className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover 
-            md:rounded-lg md:w-[500px] md:h-[600px] xl:w-[1000px] xl:h-[700px]'
+            className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
+            md:rounded-lg md:w-[500px] md:h-[350px] xl:w-[1000px] xl:h-[400px]'
             src={urlFor(project?.image).url()}
              alt="" />
             <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
